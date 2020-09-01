ac2fcec4907084d02cf645c6c1aaa33bed034e91
yaishu <68405237+yaishu@users.noreply.github.com>
2020-09-01 12:47:40 -0400

    Highest Score Added

--- sketch.js
+++ sketch.js
@@ -72,7 +72,7 @@ function draw() {
   //trex.debug = true;
   background(255);
   text("T-rex Score: "+ score, 500,50);
-  
+  text("HI: "+localStorage["HighestScore"],450,50);
   if (gameState===PLAY){
     score = score + Math.round(getFrameRate()/60);
     ground.velocityX = -(6 + 3*score/100);
